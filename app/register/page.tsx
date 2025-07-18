e1a48b0a8d1af2df0c3db02c8d1f43d77e40d7fe
Feymovs <saidaurum@gmail.com>
2025-07-19 00:30:43 +0500

    Добавлен компонент на страницу /register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,1 +1,10 @@
 export const dynamic = "force-dynamic";
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Регистрация</h1>
+      <p>Это страница регистрации.</p>
+    </div>
+  );
+}
